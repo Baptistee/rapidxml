bc7ea2e4e999e9c41283524e320bbb1fb2e28fc0
Baptistee [LAPTOP] <bbtcpp@gmail.com>
2021-07-15 16:05:01 +0200

    [Cmake] Turning rapidxml into static library

---
+++ rapidxml.cpp
@@ -0,0 +1,9 @@
+#define RAPIDXML_IMPLEMENTATION
+#define RAPIDXML_PRINT_IMPLEMENTATION
+#define RAPIDXML_UTILS_IMPLEMENTATION
+#define RAPIDXML_ITERATORS_IMPLEMENTATION
+
+#include "include/rapidxml.hpp"
+#include "include/rapidxml_print.hpp"
+#include "include/rapidxml_utils.hpp"
+#include "include/rapidxml_iterators.hpp"
